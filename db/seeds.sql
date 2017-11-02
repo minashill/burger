f5b55d88ecb4227ad009dbdad6d720fdc2903087
minashill <minashill@yahoo.com>
2017-11-02 17:05:05 -0600

    adding local files

---
+++ db/seeds.sql
@@ -0,0 +1,3 @@
+INSERT INTO burgers (burger_name, devoured) VALUES ('Plain', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Cheeseburger', false);
+INSERT INTO burgers (burger_name, devoured) VALUES ('Mushroom Burger', false);
