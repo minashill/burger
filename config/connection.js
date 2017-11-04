cc2fea89eaa9a44c265b35e81f288cdc95a6432f
minashill <minashill@yahoo.com>
2017-11-04 14:01:59 -0600

    updates

--- config/connection.js
+++ config/connection.js
@@ -4,14 +4,19 @@ var mysql = require("mysql");
 var connection;
 
 if(process.env.JAWSDB_URL){
-    connection = mysql.createConnection(process.env.JAWSDB_URL);
+    connection = mysql.createConnection({
+
+        host:'x3ztd854gaa7on6s.cbetxkdyhwsb.us-east-1.rds.amazonaws.com',
+        user:"eixmxjn54wzet91n",
+        password:'zcwx3gr9t71ws45n',
+        database:"burger_db"});
 }
 else{
     connection = mysql.createConnection({
         host:'localhost',
         user:"newuser",
         password:'1234',
-        database:"burger_db"
+        database:"burger_db"      
     });
 }
 
@@ -25,6 +30,7 @@ connection.connect(function(err) {
   console.log("connected as id " + connection.threadId);
 });
 
+connection.end();
 // Export connection for our ORM to use.
 module.exports = connection;
 // 
