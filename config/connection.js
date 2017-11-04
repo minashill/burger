9e68f8bb94a3beb5365fe6701589cb209142025c
minashill <minashill@yahoo.com>
2017-11-04 13:19:55 -0600

    updates

--- config/connection.js
+++ config/connection.js
@@ -4,6 +4,13 @@ var connection = mysql.createConnection(process.env.JAWSDB_URL);
 
 connection.connect();
 
+connection.query('SELECT 1 + 1 AS solution', function(err, rows, fields) {
+  if (err) throw err;
+
+  console.log('The solution is: ', rows[0].solution);
+});
+
+connection.end();
 
 // Export connection for our ORM to use.
 module.exports = connection;
